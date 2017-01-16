ae4f0aa5f9413959e2df73c07c43dc4a6c3a681d
Dalmo Cirne <dalmo@mparticle.com>
2017-01-16 13:26:14 -0500

    Remove scheduling of local notifications

--- mParticle-Apple-SDK/MPBackendController.mm
+++ mParticle-Apple-SDK/MPBackendController.mm
@@ -2926,10 +2926,6 @@ static BOOL appBackgrounded = NO;
 #pragma mark MPNotificationControllerDelegate
 - (void)receivedUserNotification:(MParticleUserNotification *)userNotification {
     switch (userNotification.command) {
-        case MPUserNotificationCommandAlertUserLocalTime:
-            [self.notificationController scheduleNotification:userNotification];
-            break;
-            
         case MPUserNotificationCommandConfigRefresh:
             [self requestConfig:nil];
             break;
